a76d6decf5d6d53b9e248f24551954a47899c716
root <124867543+gpt-partners@users.noreply.github.com>
2023-11-22 19:31:02 +0000

    add mocks

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,27 @@
 from fastapi.testclient import TestClient
+from llm.create_vito import VitoRequest
 
 from .main import app
 
 client = TestClient(app)
 
+vr = VitoRequest(linked_in_url="", company_url="")
 
-def test_get_main():
+
+class MockOpenAI:
+    def invoke(_text):
+        return vr
+
+
+def test_get_main(mocker):
+    mocker.patch("llm.create_vito.llm", MockOpenAI)
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
 
 
-def test_post_vito():
+def test_post_vito(mocker):
+    mocker.patch("llm.create_vito.llm", MockOpenAI)
     response = client.post(
         "/vito",
         json={
